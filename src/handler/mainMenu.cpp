289cdd92cc42c780dff5c9b1faa6a0791b2217d0
JuanJakobo <github@johnnsen.tech>
2021-11-21 13:55:50 +0100

    add mainMenu

---
+++ src/handler/mainMenu.cpp
@@ -0,0 +1,68 @@
+//------------------------------------------------------------------
+// mainMenu.cpp
+//
+// Author:           JuanJakobo
+// Date:             14.06.2020
+//
+//-------------------------------------------------------------------
+
+#include "inkview.h"
+#include "mainMenu.h"
+
+#include <string>
+
+using std::string;
+
+MainMenu::MainMenu(const string &name)
+{
+    _panelMenuHeight = ScreenHeight() / 18;
+    _mainMenuWidth = ScreenWidth() / 3;
+    _panelMenuBeginY = 0;
+    _panelMenuBeginX = ScreenWidth() - _mainMenuWidth;
+
+    _menuButtonRect = iRect(_mainMenuWidth * 2, _panelMenuBeginY, _mainMenuWidth, _panelMenuHeight, ALIGN_RIGHT);
+
+    _menuFont = OpenFont("LiberationMono-Bold", _panelMenuHeight / 2, FONT_STD);
+
+    SetFont(_menuFont, BLACK);
+    DrawTextRect(0, _panelMenuBeginY, ScreenWidth(), _panelMenuHeight, name.c_str(), ALIGN_CENTER);
+    DrawTextRect2(&_menuButtonRect, "Menu");
+    DrawLine(0, _panelMenuHeight - 1, ScreenWidth(), _panelMenuHeight - 1, BLACK);
+
+    _contentRect = iRect(0, _panelMenuHeight, ScreenWidth(), (ScreenHeight() - _panelMenuHeight), 0);
+    
+    SetPanelType(0);
+    PartialUpdate(0, _panelMenuBeginY, ScreenWidth(), _panelMenuHeight);
+}
+
+MainMenu::~MainMenu()
+{  
+    CloseFont(_menuFont);
+    free(_menu);
+    free(_info);
+    free(_syncDownloaded);
+    free(_showDownloaded);
+    free(_showUnread);
+    free(_showStarred);
+    free(_markAsReadTillPage);
+    free(_exit);
+}
+
+int MainMenu::createMenu(bool mainView, const iv_menuhandler &handler)
+{
+    imenu mainMenu[] =
+        {
+            {ITEM_HEADER, 0, _menu, NULL},
+            {mainView ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 101, _showDownloaded, NULL},
+            {mainView ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 102, _showUnread, NULL},
+            {mainView ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 103, _showStarred, NULL},
+            {mainView ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 104, _syncDownloaded, NULL},
+            {mainView ? (short)ITEM_ACTIVE : (short)ITEM_HIDDEN, 105, _markAsReadTillPage, NULL},
+            {ITEM_ACTIVE, 107, _info, NULL},
+            {ITEM_ACTIVE, 108, _exit, NULL},
+            {0, 0, NULL, NULL}};
+
+    OpenMenu(mainMenu, 0, _panelMenuBeginX, _panelMenuBeginY, handler);
+
+    return 1;
+}
---
+++ src/handler/mainMenu.h
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------
+// mainMenu.h
+//
+// Author:           JuanJakobo
+// Date:             14.06.2020
+// Description:      Handles the menubar and the menu
+//-------------------------------------------------------------------
+
+#ifndef MAIN_MENU
+#define MAIN_MENU
+
+#include "inkview.h"
+
+#include <string>
+
+class MainMenu
+{
+public:
+    /**
+        * Defines fonds, sets global Event Handler and starts new content 
+        * 
+        * @param name name of the application
+        */
+    MainMenu(const std::string &name);
+
+    ~MainMenu();
+
+    irect *getContentRect() { return &_contentRect; };
+    irect *getMenuButtonRect() { return &_menuButtonRect; };
+
+    /**
+        * Shows the menu on the screen, lets the user choose menu options and then redirects the handler to the caller
+        * 
+        * @param mainView if true mainView will be shown
+        * @return int returns if the event was handled
+        */
+    int createMenu(bool mainView, const iv_menuhandler &handler);
+
+private:
+    ifont *_menuFont;
+
+    int _panelMenuBeginX;
+    int _panelMenuBeginY;
+    int _panelMenuHeight;
+    int _mainMenuWidth;
+    irect _menuButtonRect;
+
+    imenu _mainMenu;
+    irect _contentRect;
+
+    char *_menu = strdup("Menu");
+    char *_syncDownloaded = strdup("Sync marked");
+    char *_showUnread = strdup("Show unread");
+    char *_showStarred = strdup("Show starred");
+    char *_showDownloaded = strdup("Show downloaded");
+    char *_markAsReadTillPage = strdup("Mark as read till page");
+    char *_info = strdup("Info");
+    char *_exit = strdup("Close App");
+
+};
+#endif
